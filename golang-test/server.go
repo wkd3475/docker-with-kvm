64cade8d9ec19674d7deab697cc02199ab6fb243
wkd3475 <wkd3475@gmail.com>
2020-03-02 14:30:17 +0900

    feat: create golang api server

---
+++ golang-test/server.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+	"log"
+	"strconv"
+)
+
+func Test(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	var result int64
+	result = 0
+	maxValue, _ := strconv.ParseInt(ps.ByName("id"), 10, 32)
+	var i int64
+	for i = 0; i < maxValue; i++ {
+		result = result + 1
+	}
+	message := strconv.FormatInt(result, 10)
+	w.Write([]byte(message))
+}
+
+func main() {
+	router := httprouter.New()
+	router.GET("/api/test/:id", Test)
+
+	log.Fatal(http.ListenAndServe(":3001", router))
+}
