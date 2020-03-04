ffe367d7d302e831f283f26b2f5ff8c17c9f401c
wkd3475 <wkd3475@gmail.com>
2020-03-04 16:09:08 +0900

    feat: Add cli options

--- request/client.js
+++ request/client.js
@@ -1,16 +1,57 @@
+if (process.argv.length!=5) {
+	console.log("you need argvs");
+	console.log("ex) node client.js <language> <request_num> <load_strength>");
+	console.log("<language>");
+	console.log(": java, python, javascript, go");
+	console.log("<request_num>");
+	console.log(": 1~2000");
+	console.log("<load_strength>");
+	console.log(": 1~2^63");
+	process.exit();
+}
+
+let PORT_NUM, REQUEST_NUM, LOAD_STRENGTH
+
+if (process.argv[2]=="javascript") {
+	PORT_NUM = 3000;
+} else if (process.argv[2]=="go") {
+	PORT_NUM = 3001;
+} else if (process.argv[2]=="python") {
+	PORT_NUM = 3002;
+} else if (process.argv[2]=="java") {
+	PORT_NUM = 3003;
+} else {
+	console.log("wrong language");
+	process.exit();
+}
+
+if (Number(process.argv[3])<0 || Number(process.argv[3])>2000) {
+	console.log("wrong request_num(1~2000)");
+	process.exit();
+} else {
+	REQUEST_NUM = Number(process.argv[3]);
+}
+
+if (Number(process.argv[4]<0) || Number(process.argv[4])>Math.pow(2, 63)) {
+	console.log("wrong load_strength(1~2^63)");
+	process.exit();
+} else {
+	LOAD_STRENGTH = Number(process.argv[4]);
+}
+
+console.log("request_num :"+REQUEST_NUM);
+console.log("load_strength :"+LOAD_STRENGTH);
+
 const Client = require('node-rest-client').Client;
 const client = new Client();
 
-const TEST_NUM = 2000;
-const TEST_SIZE = 1000;
-
 let startTime = new Date().getTime();
-for (var i=1; i<TEST_NUM; i++) {
-	client.get("http://localhost:3001/api/test/"+TEST_SIZE, (data, response) => {
+for (var i=1; i<REQUEST_NUM; i++) {
+	client.get("http://localhost:3001/api/test/"+LOAD_STRENGTH, (data, response) => {
 	});
 }
 
-client.get("http://localhost:3001/api/test/"+TEST_SIZE, (data, response) => {
+client.get("http://localhost:3001/api/test/"+LOAD_STRENGTH, (data, response) => {
 	let endTime = new Date().getTime();
 	console.log("total time : "+(endTime-startTime));
 });
