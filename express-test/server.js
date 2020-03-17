e8986fb26f968576a69136d7e0ae219ac1288961
wkd3475 <wkd3475@gmail.com>
2020-03-17 19:06:56 +0900

    feat: I/O 관련 기능 추가 및 기존 CPU 기능의 복잡성을 증가

--- express-test/server.js
+++ express-test/server.js
@@ -1,13 +1,66 @@
 const express = require("express");
-const sleep = require('sleep');
+const fs = require('fs');
 const app = express();
+
+let bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 app.get("/api/cpu/:id", (req, res) => {
 	let result = 0;
 	for (let i=0; i<parseInt(req.params.id); i++) {
-		result = result + 1;
+		//result = result + 1;
+		result = ((result + Date.now())*Math.sqrt(Date.now()))%123456789;
 	}
 	res.send([result]);
+});
+
+app.get("/api/makefile/:id", (req, res) => {
+	fs.stat("./json/bigfile"+req.params.id+".json", (err) => {
+		if (!err) {
+			console.log("file or directory exists");
+			res.send(["already exist!"]);
+		}
+		else if (err.code === 'ENOENT') {
+			let obj = {};
+			for (let i=1; i<=parseInt(req.params.id); i++) {
+				obj["data"+i] = "val"+i;
+			}
+			let stringJson = JSON.stringify(obj);
+			fs.open("./json/bigfile"+req.params.id+".json", 'a', "666", (err, id) => {
+				if (err) {
+					console.log("file open err!");
+				} else {
+					fs.write(id, stringJson, null, 'utf8', (err) => {
+						console.log("file was saved!");
+					});
+				}
+			});
+			res.send([req.params.id]);
+		}
+	});
+});
+
+app.post("/api/jsonio/:id", (req, res, next) => {
+	let body = req.body;
+	let writer = fs.createWriteStream("./file/bigfile"+req.params.id+".txt");
+	for (key in body) {
+		writer.write(key+":"+body[key]+"\n");
+	}
+	writer.end();
+	writer.on('finish', () => {});
+	console.log('finish'+req.params.id);
+	res.send([req.params.id]);
+});
+
+app.get("/api/io/:id", (req, res) => {
+	let writer = fs.createWriteStream("./file/bigfile"+Date.now()+".txt");
+	for (let i=1; i<=parseInt(req.params.id); i++) {
+		writer.write("server #"+i+"\n");
+	}
+	writer.end();
+	writer.on('finish', () => {
+	});
+	res.send([req.params.id]);
 });
 
 app.listen(3000, () => console.log('start'));
